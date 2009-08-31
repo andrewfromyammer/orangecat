76f3ca3a9120973a665a3dc89e2fa4a125d8439d
aa <aa@localhost.(none)>
2009-08-30 22:05:42 -0700

    window title

---
+++ OrangeCat.h
@@ -0,0 +1,11 @@
+#import <Cocoa/Cocoa.h>
+
+
+@interface OrangeCat : NSObject {
+  IBOutlet NSWindow* window;
+  IBOutlet NSTextField* authCode;
+  IBOutlet id firstResponder;
+
+}
+
+@end
---
+++ OrangeCat.m
@@ -0,0 +1,13 @@
+
+
+#import "OrangeCat.h"
+//#import "LocalStorage.h"
+
+@implementation OrangeCat
+
+- (void)applicationDidFinishLaunching:(NSNotification*)notification {
+  window.title = @"Orange Cat";
+  
+}
+
+@end
